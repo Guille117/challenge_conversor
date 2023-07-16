53fc8559717ed0ce3f64a6e848c4144f52727ac4
Guillermo Bixcul <bixculg7@gmail.com>
2023-07-16 12:52:19 -0600

    1

---
+++ src/gt/com/conversor/prinipal/Principal.java
@@ -0,0 +1,16 @@
+
+package gt.com.conversor.prinipal;
+
+import gt.com.conversor.ventanas.Ventana;
+
+/**
+ *
+ * @author Gui
+ */
+public class Principal {
+    public static void main(String[] args) {
+      
+        Ventana v1 = new Ventana();
+        v1.setVisible(true);
+    }
+}
---
+++ src/gt/com/conversor/ventanas/Ventana.java
@@ -0,0 +1,16 @@
+package gt.com.conversor.ventanas;
+
+/**
+ *
+ * @author Gui
+ */
+public class Ventana  extends javax.swing.JFrame{
+    public  Ventana(){                                                                              // contructor de ventana
+        this.setSize(750, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        
+    }
+    
+    
+}
